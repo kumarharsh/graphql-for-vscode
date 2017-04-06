68084f5c42ed87373eaf3a19de3a97c5c5560a94
Kumar Harsh <khs@playlyfe.com>
2017-04-07 05:25:17 +0530

    fix(client): show status-bar icons when server starts

--- src/client/extension.ts
+++ src/client/extension.ts
@@ -15,9 +15,9 @@ import * as path from 'path';
 
 const extName = 'graphqlForVSCode';
 enum Status {
+  init = 0,
   ok = 1,
-  warn = 2,
-  error = 3,
+  error = 2,
 }
 
 export function activate(context: ExtensionContext) {
@@ -83,59 +83,66 @@ export function activate(context: ExtensionContext) {
 function graphqlActivate(context: ExtensionContext, client: LanguageClient) {
   // Instantiate status bar item on the right most side on status bar
   let statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 0);
-  let graphqlStatus: Status = Status.ok;
+  let graphqlStatus: Status = Status.init;
   let serverRunning: boolean = false;
   // the text to display on status bar item
   const text = 'GQL';
   // The icon of the status bar item
   const icons = {
-    [Status.ok]: '$(zap) ',
-    [Status.warn]: '$(alert) ',
-    [Status.error]: '$(stop) ',
+    [Status.init]: 'sync',
+    [Status.ok]: 'plug',
+    [Status.error]: 'stop',
   }
-  statusBarItem.text = text;
+  statusBarItem.text = `$(${icons[graphqlStatus]}) ${text}`;
+  statusBarItem.tooltip = 'Server is initializing';
   statusBarItem.command = 'graphqlForVSCode.showOutputChannel';
   context.subscriptions.push(statusBarItem);
 
   function showStatusBarItem(show: boolean): void {
     if (show) {
       statusBarItem.show();
-    } else {
-      statusBarItem.hide();
+      return;
     }
+    statusBarItem.hide();
   }
 
-  function updateStatus(status: Status) {
-		switch (status) {
+  function updateStatusBarColour(): void {
+		switch (graphqlStatus) {
 			case Status.ok:
 				statusBarItem.color = undefined;
-				break;
-			case Status.warn:
-				statusBarItem.color = 'yellow';
 				break;
 			case Status.error:
 				statusBarItem.color = 'red';
 				break;
 		}
-		graphqlStatus = status;
 	}
 
   function updateStatusBarVisibility(editor: TextEditor): void {
-		statusBarItem.text = icons[graphqlStatus] + text;
-    updateStatus(graphqlStatus);
-		showStatusBarItem(
-			// serverRunning &&
-			(
-				graphqlStatus !== Status.ok ||
-				(editor && editor.document.languageId === 'graphql')
-			)
-		);
+		statusBarItem.text = `$(${icons[graphqlStatus]}) ${text}`;
+    updateStatusBarColour();
+		showStatusBarItem(editor && editor.document.languageId === 'graphql');
 	}
 
-	// TODO unsure how client's state change
   context.subscriptions.push(client.start());
+  client.onDidChangeState((event) => {
+		if (event.newState === ClientState.Running) {
+			statusBarItem.tooltip = 'Graphql language server is running';
+      graphqlStatus = Status.ok;
+			serverRunning = true;
+		} else {
+			client.info('The graphql server has stopped running');
+			statusBarItem.tooltip = 'Graphql language server has stopped';
+      graphqlStatus = Status.error;
+			serverRunning = false;
+		}
+		updateStatusBarVisibility(window.activeTextEditor);
+	});
 
-  window.onDidChangeActiveTextEditor(updateStatusBarVisibility);
-  updateStatusBarVisibility(window.activeTextEditor)
-
+  window.onDidChangeActiveTextEditor((...args) => {
+    if (serverRunning) { // update the status if the server is running
+      graphqlStatus = Status.ok;
+    }
+    updateStatusBarVisibility(window.activeTextEditor);
+  });
+  updateStatusBarVisibility(window.activeTextEditor);
 }
