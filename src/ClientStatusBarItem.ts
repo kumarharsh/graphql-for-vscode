774628f10a39e66fd3de9c59b14f4fc3cd5c8348
Mayank Agarwal <mayank1791989@gmail.com>
2018-05-01 20:13:59 +0530

    feat(server): use gql-language-server
    - adds support for multi-root workspaces. Closes #33
    - interfaces with @playlyfe/gql v3. Closes #83
    - can auto-download gql library if not found. Addresses #16
    
    other changes:
    - update language server to v4.
    - add prettier and format extension code.
    
    BREAKING_CHANGES: the extension options have changed.
    You'll need to go into your settings page, and configure these options:
    * graphqlForVSCode.autoDownloadGQL: auto download @playlyfe/gql if not
    * graphqlForVSCode.gqlPath: An absolute path to @playlyfe/gql module, if
    not auto-downloaded. If the library is located at the path
    `${workspaceRoot}/src/ui/node_modules/@playlyfe/gql`, you need to set
    the gqlPath as `${workspaceRoot}/src/ui`.
    * graphqlForVSCode.configDir: An absolute path to directory containing
    the `.gqlConfig` file. You can use `${workspaceRoot}` to specify the
    root.
    * graphqlForVSCode.watchman: use fb-watchman to watch files _if available_

---
+++ src/ClientStatusBarItem.ts
@@ -0,0 +1,144 @@
+'use strict';
+
+import {
+  StatusBarAlignment,
+  window,
+  StatusBarItem,
+  workspace,
+  TextEditor,
+  languages,
+  TextDocument,
+  commands,
+  Disposable,
+} from 'vscode';
+
+import { LanguageClient, State } from 'vscode-languageclient';
+
+enum Status {
+  init = 1,
+  ok = 2,
+  error = 3,
+}
+
+const STATUS_BAR_ITEM_NAME = 'GQL';
+const STATUS_BAR_UI = {
+  [Status.init]: {
+    icon: 'sync',
+    color: 'white',
+    tooltip: 'Graphql language server is initializing.',
+  },
+  [Status.ok]: {
+    icon: 'plug',
+    color: 'while',
+    tooltip: 'Graphql language server is running.',
+  },
+  [Status.error]: {
+    icon: 'stop',
+    color: 'red',
+    tooltip: 'Graphql language server is not running.',
+  },
+};
+export default class ClientStatusBarItem {
+  private _item: StatusBarItem;
+  private _client: LanguageClient;
+  private _disposables: { dispose: () => any }[] = [];
+
+  constructor(client: LanguageClient) {
+    this._item = window.createStatusBarItem(StatusBarAlignment.Right, 0);
+    this._client = client;
+
+    this._disposables.push(this._item);
+    this._disposables.push(this._addOnClickToShowOutputChannel());
+
+    // update status bar depending on client state
+    this._setStatus(Status.init);
+    this._registerStatusChangeListeners();
+
+    // update visibility of statusBarItem depending on current activeTextEditor
+    this._updateVisibility(window.activeTextEditor);
+    window.onDidChangeActiveTextEditor(this._updateVisibility);
+  }
+
+  public dispose() {
+    this._disposables.forEach(item => {
+      item.dispose();
+    });
+    this._item = null;
+    this._client = null;
+  }
+
+  private _registerStatusChangeListeners() {
+    this._client.onDidChangeState(({ oldState, newState }) => {
+      if (newState === State.Running) {
+        this._setStatus(Status.ok);
+      } else if (newState === State.Stopped) {
+        this._setStatus(Status.error);
+      }
+    });
+
+    this._client.onReady().then(
+      () => {
+        this._setStatus(Status.ok);
+      },
+      () => {
+        this._setStatus(Status.error);
+      },
+    );
+  }
+
+  private _addOnClickToShowOutputChannel() {
+    const commandName = `showOutputChannel-${this._client.outputChannel.name}`;
+    const disposable = commands.registerCommand(commandName, () => {
+      this._client.outputChannel.show();
+    });
+    this._item.command = commandName;
+    return disposable;
+  }
+
+  private _updateVisibility = (textEditor: TextEditor) => {
+    let hide = true;
+
+    if (textEditor && this._checkDocumentInsideWorkspace(textEditor.document)) {
+      if (this._client.initializeResult) {
+        // if client is initialized than show only for file extensions defined in .gqlconfig
+        // @TODO: if possible match against patterns in .gqlconfig instead of extensions.
+        const extensions = this._client.initializeResult.fileExtensions;
+        const score = languages.match(
+          { scheme: 'file', pattern: `**/*.{${extensions.join(',')}}` },
+          textEditor.document,
+        );
+        hide = score === 0;
+      } else {
+        // while server is initializing show status bar item
+        // for all files inside worspace
+        hide = false;
+      }
+    }
+
+    hide ? this._hide() : this._show();
+  };
+
+  private _checkDocumentInsideWorkspace(document: TextDocument): boolean {
+    const folder = workspace.getWorkspaceFolder(document.uri);
+    return folder && folder.uri.toString() === this._getWorkspace();
+  }
+
+  private _getWorkspace(): string {
+    return this._client.clientOptions.workspaceFolder.uri.toString();
+  }
+
+  private _show() {
+    this._item.show();
+  }
+
+  private _hide() {
+    this._item.hide();
+  }
+
+  private _setStatus(status: Status) {
+    const ui = STATUS_BAR_UI[status];
+    this._item.text = `$(${ui.icon}) ${STATUS_BAR_ITEM_NAME}`;
+    this._item.tooltip = ui.tooltip;
+    this._item.color = ui.color;
+  }
+}
---
+++ src/extension.ts
@@ -0,0 +1,177 @@
+'use strict';
+
+import * as path from 'path';
+import {
+  commands,
+  ExtensionContext,
+  StatusBarAlignment,
+  TextEditor,
+  window,
+  TextDocument,
+  workspace as Workspace,
+  WorkspaceFolder,
+} from 'vscode';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  NotificationType,
+  ServerOptions,
+  State as ClientState,
+  TransportKind,
+} from 'vscode-languageclient';
+
+import ClientStatusBarItem from './ClientStatusBarItem';
+import { findConfigFile as findGQLConfigFile } from '@playlyfe/gql-language-server';
+
+const EXT_NAME = 'graphqlForVSCode';
+const GQL_LANGUAGE_SERVER_CLI_PATH = require.resolve(
+  '@playlyfe/gql-language-server/lib/bin/cli',
+);
+
+interface Client {
+  dispose: () => any;
+  statusBarItem: ClientStatusBarItem;
+  client: LanguageClient;
+}
+
+const clients: Map<string, Client | null> = new Map();
+let activeStatusBarItem: ClientStatusBarItem = null;
+
+export function activate(context: ExtensionContext) {
+  createClientForWorkspaces();
+  // update clients when workspaceFolderChanges
+  Workspace.onDidChangeWorkspaceFolders(createClientForWorkspaces);
+}
+
+export function deactivate(): Thenable<void> {
+  let promises: Thenable<void>[] = [];
+  clients.forEach(client => {
+    promises.push(client.dispose());
+  });
+  return Promise.all(promises).then(() => undefined);
+}
+
+function createClientForWorkspaces() {
+  const workspaceFolders = Workspace.workspaceFolders || [];
+  const workspaceFoldersIndex = {};
+
+  workspaceFolders.forEach(folder => {
+    const key = folder.uri.toString();
+    if (!clients.has(key)) {
+      const client = createClientForWorkspace(folder);
+      console.log('adding client', key, client);
+      clients.set(key, client);
+    }
+    workspaceFoldersIndex[key] = true;
+  });
+
+  // remove clients for removed workspace folders
+  clients.forEach((client, key) => {
+    // remove client
+    if (!workspaceFoldersIndex[key]) {
+      console.log('deleting client', key);
+      clients.delete(key);
+      if (client) {
+        client.dispose();
+      }
+    }
+  });
+}
+
+function createClientForWorkspace(folder: WorkspaceFolder): null | Client {
+  // per workspacefolder settings
+  const config = Workspace.getConfiguration(EXT_NAME, folder.uri);
+  const outputChannel = window.createOutputChannel(`GraphQL - ${folder.name}`);
+
+  const gqlconfigDir = resolvePath(config.get<string>('configDir'), folder);
+
+  // check can activate gql plugin
+  // if config found in folder then activate
+  try {
+    findGQLConfigFile(gqlconfigDir);
+  } catch (err) {
+    outputChannel.appendLine(
+      `Not activating language-server for workspace folder '${folder.name}'.\n` +
+      `Reason: ${err.message}`,
+    );
+    return null;
+  }
+
+  const gqlLanguageServerCliOptions = [
+    `--watchman=${config.get<boolean>('watchman', true)}`,
+    `--auto-download-gql=${config.get<boolean>('autoDownloadGQL')}`,
+    `--config-dir=${gqlconfigDir}`,
+    `--gql-path=${resolvePath(config.get('gqlPath'), folder)}`,
+    `--loglevel=${config.get('loglevel')}`,
+  ];
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: {
+      module: GQL_LANGUAGE_SERVER_CLI_PATH,
+      transport: TransportKind.ipc,
+      args: gqlLanguageServerCliOptions,
+    },
+    debug: {
+      module: GQL_LANGUAGE_SERVER_CLI_PATH,
+      transport: TransportKind.ipc,
+      options: {
+        execArgv: ['--nolazy', '--debug=6004'],
+      },
+      args: gqlLanguageServerCliOptions,
+    },
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    diagnosticCollectionName: 'graphql',
+    initializationFailedHandler: error => {
+      window.showErrorMessage(
+        `Plugin 'graphql-for-vscode' couldn't start for workspace '${
+          folder.name
+        }'. See output channel '${folder.name}' for more details.`,
+      );
+      client.error('Server initialization failed:', error.message);
+      client.outputChannel.show(true);
+      // avoid retries
+      return false;
+    },
+    outputChannel: outputChannel,
+    workspaceFolder: folder,
+  };
+
+  // Create the language client and start the client.
+  const client = new LanguageClient(
+    'Graphql For VSCode',
+    serverOptions,
+    clientOptions,
+  );
+
+  const statusBarItem = new ClientStatusBarItem(client);
+
+  const subscriptions = [
+    client.start(),
+    {
+      dispose() {
+        outputChannel.hide();
+        outputChannel.dispose();
+      },
+    },
+    statusBarItem,
+  ];
+
+  return {
+    dispose: () => {
+      subscriptions.forEach(subscription => {
+        subscription.dispose();
+      });
+    },
+    statusBarItem,
+    client,
+  };
+}
+
+function resolvePath(path: string, folder: WorkspaceFolder): string {
+  return path.replace('${workspaceRoot}', folder.uri.fsPath);
+}
